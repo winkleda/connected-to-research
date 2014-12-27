179a7cfeecc258627022e383637a8e7d83ee0149
tstevens10182 <tstevens10182@gmail.com>
2014-12-26 16:36:43 -0800

    Publication column now generated by template usuing and angular and pulling informatino from publication-items.json

--- js/app.js
+++ js/app.js
@@ -8,14 +8,29 @@
 			templateUrl:"tpl/filter.tpl.html"
 		};
 	});
+
 	
+	app.directive('publicationItem', function(){
+		return{
+			restrict:'E',
+			templateUrl:'tpl/publication-item.tpl.html'
+		};
+	});
+
+
 	app.controller('PublicationController',['$http', function($http){
 		var publicationCtrl = this;
+	
+
 		publicationCtrl.filter = [];
 		$http.get("test/filter.json").success(function(data){
 			publicationCtrl.filter = data;
 		});
-	
+
+		publicationCtrl.publicationItems = [];
+		$http.get("test/publication-items.json").success(function(data){
+			publicationCtrl.publicationItems = data;
+		});	
 	}]);
 	
 })();
