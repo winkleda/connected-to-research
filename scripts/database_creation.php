ed69709c4dabd468736bd0991404162c54734ca2
tstevens10182 <tstevens10182@gmail.com>
2015-01-01 19:57:33 -0800

    changed the database_structure doc to correspond with the structure created by the database_creation script

--- scripts/database_creation.php
+++ scripts/database_creation.php
@@ -19,22 +19,88 @@ $br = "<br/>";
 // the user table creation sql 
 $sql_create_user = "CREATE TABLE user (
 	user_id int unsigned auto_increment primary key,
-	name_f varchar not null,
-	name_l varchar not null,
-	email  varchar not null,
-	password varchar not null,
-	user_img_src varchar,
-	user_occ varchar
+	name_f varchar(255) not null,
+	name_l varchar(255) not null,
+	email  varchar(255) not null,
+	password varchar(255) not null,
+	user_img_src varchar(255),
+	user_occ varchar(255)
 )";
 
 //the user article link table creation sql
 $sql_create_user_article_link = "CREATE TABLE user_article_link (
 	id int unsigned auto_increment primary key,
-	article_id int not null,
-	user_id int not null,
-	status varchar not null
+	article_id int unsigned not null,
+	user_id int unsigned not null,
+	status varchar(255) not null
 )";
 
+// the create article sql
+$sql_create_article = "CREATE TABLE article (
+	type varchar(255),
+	id int unsigned auto_increment primary key,
+	title text not null,
+	a_date date,
+	keywords text,
+	reprint varchar(255),
+	startpage mediumint unsigned,
+	endpage mediumint unsigned,
+	j_name varchar(255),
+	article_id mediumint unsigned,
+	j_issue smallint unsigned,
+	j_volume smallint unsigned,
+	abstract text,
+	url varchar(255),
+	isbn_issn varchar(255),
+	lang varchar(255),
+	discipline varchar(255)
+)";
+
+
+// the user research and events link create table sql
+$sql_create_user_red_link = "CREATE TABLE user_red_link (
+	research_id int unsigned,
+	user_id int unsigned,
+	id int unsigned auto_increment primary key
+)";
+
+// the research events and deadlines create table sql
+$sql_create_re_deadlines = "CREATE TABLE re_deadlines (
+	re_date date,
+	re_id int unsigned auto_increment primary key,
+	title varchar(255) not null,
+	location varchar(255),
+	status varchar(255)
+)";
+
+// the call for participation create table sql
+$sql_create_call_for_part = "CREATE TABLE call_for_part (
+	p_date date,
+	title varchar(255),
+	location varchar(255),
+	p_id int unsigned auto_increment primary key,
+	impact_fact mediumint unsigned,
+	description varchar(255)
+)";
+
+// call for participation item table creation sql
+$sql_create_call_part_item = "CREATE TABLE call_part_item (
+	title varchar(255),
+	i_date date,
+	link_id int unsigned,
+	i_id int unsigned auto_increment primary key
+)";
+
+
+$sql_table_creation_array = array(
+	$sql_create_user,
+	$sql_create_user_article_link,
+	$sql_create_article,
+	$sql_create_user_red_link,
+	$sql_create_re_deadlines,
+	$sql_create_call_for_part,
+	$sql_create_call_part_item
+);
 
 
 
@@ -65,19 +131,17 @@ if ($conn->connect_error){
 }
 echo "Connected Successfully to ". $db_name . $br;
 
-$sql = "CREATE TABLE MyTable (
-	id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	firstname VARCHAR(30) NOT NULL,
-	lastname VARCHAR(30) NOT NULL,
-	email VARCHAR(50),
-	reg_dat TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-	echo "table mytable created successfully".$br;
-}else{
-	echo "error creating table: ". $conn->error . $br;
+//
+$i = 1;
+foreach ($sql_table_creation_array as $sql){
+	if ($conn->query($sql) === TRUE) {
+		echo "table " .$i. "  create successfully".$br;
+	}else{
+		echo "error creating table " .$i. ": ". $conn->error . $br;
+	}
+	$i++;
 }
+
 $conn->close();
 
 ?>
