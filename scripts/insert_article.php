efe2e4ab7e50e4e7aaca05b5212caa63869f119d
Anton Dovzhik <dovzhika@oregonstate.edu>
2015-01-18 23:24:40 -0800

    Example XML docs are inserted into database

---
+++ scripts/insert_article.php
@@ -0,0 +1,57 @@
+<?php
+include 'connection.php';
+include 'parse.php'; /* Returns array of parsed articles */
+
+/* Specifies which XML file should be read and inserted into database */
+$articles = parseArticle("xml_docs/example2.xml");
+
+/* Iterates through each article and adds it to database */
+/* NOTE: lang and discipline attributes are not added! */
+foreach ($articles as $article) {
+	if (!($stmt = $mysqli->prepare(
+		"INSERT INTO ctr_article(
+			article_id,
+			startpage,
+			endpage,
+			j_issue,
+			j_volume, 
+			type,
+			title,
+			a_date,
+			keywords,
+			reprint, 
+			j_name,
+			abstract,
+			url,
+			isbn_issn) 
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"))) {
+		
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+
+	if (!$stmt->bind_param("iiiiisssssssss", 
+		$article->id,
+		$article->startpage,
+		$article->endpage,
+		$article->issue,
+		$article->volume,
+		$article->type,
+		$article->title, 
+		$article->date, 
+		$article->keywords,
+		$article->reprint, 
+		$article->journalfull, 
+		$article->abstract, 
+		$article->url, 
+		$article->isbnorissn)) {
+		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+
+	if (!$stmt->execute()) {
+		echo "Failed to insert article " . $article->id . ": " . $stmt->error . "<br>";
+	} else {
+		echo "Article " . $article->id . " successfully inserted into database.<br>";
+	}
+}
+
+?>
---
+++ scripts/parse.php
@@ -0,0 +1,46 @@
+<?php
+/* Source of parse: http://php.net/manual/en/function.xml-parse-into-struct.php */
+ini_set('display_errors', 1);
+
+/* Object for holding metadata of single article */
+class Article {
+	function Article ($article) {
+		foreach($article as $key=>$value)
+			$this->$key = $article[$key];
+	}
+}
+
+/* */
+function parseArticle($filename) {
+	$data = implode("", file($filename));
+    $parser = xml_parser_create();
+    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+    xml_parse_into_struct($parser, $data, $values, $tags);
+    xml_parser_free($parser);
+
+    /* Grabs all elements within <record> tag */
+    foreach ($tags as $key=>$value) {
+    	if ($key == "record") {
+    		$record = $value;
+    		for ($i = 0; $i < count($record); $i += 2) {
+    			$offset = $record[$i] + 1;
+                $len = $record[$i + 1] - $offset;
+                $articles[] = parseRecord(array_slice($values, $offset, $len));
+    		}
+    	} else {
+    		continue;
+    	}
+    }
+    return $articles;
+}
+
+/* Creates and populates Article object */
+function parseRecord($article_info) {
+    for ($i = 0; $i < count($article_info); $i++) {
+        $article[$article_info[$i]["tag"]] = $article_info[$i]["value"];
+    }
+    return new Article($article);
+}
+
+?>
