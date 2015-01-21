a99c313dde1bb838b5c70f90267bf05b9c1af7df
Anton Dovzhik <dovzhika@oregonstate.edu>
2015-01-20 22:52:37 -0800

    Individual articles for sample users

--- ajax/publication-items.php
+++ ajax/publication-items.php
@@ -2,8 +2,15 @@
 ini_set('display_errors', 'On');
 include '../scripts/connection.php';
 
+session_start();
+
+/* Find user id*/
+$sql = "SELECT user_id FROM ctr_user WHERE email = '$_SESSION[email]'";
+$result = $mysqli->query($sql);
+$user = $result->fetch_assoc();
+
 /* Queries first 3 articles from database */
-$sql = "SELECT * FROM ctr_article LIMIT 3";
+$sql = "SELECT * FROM ctr_article a, ctr_user_article_link u WHERE a.id = u.article_id AND u.user_id = '$user[user_id]'";
 $articles = $mysqli->query($sql);
 
 // structure of the data to be sent to the client
