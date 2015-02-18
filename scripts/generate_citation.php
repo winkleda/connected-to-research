eb10457c1cf006d69d1ca5d9a31689dc8ed4065d
Anton Dovzhik <dovzhika@oregonstate.edu>
2015-02-18 15:33:20 -0800

    Ajax call for citation working

--- scripts/generate_citation.php
+++ scripts/generate_citation.php
@@ -1,13 +1,17 @@
 <?php
-header("Content-type: text/plain");
-header("Content-Disposition: attachment; filename='citation.enw'");
+//header("Content-type: text/plain");
+//header("Content-Disposition: attachment; filename='enw.enw'");
 
 ini_set('display_errors', 'On');
 include 'connection.php';
 
 session_start();
 
-$article_id = 324; /* Dependent on which article click originated from */
+
+/* Uncomment this line after GET variable is set!! */
+//$article_id = $_POST['id'];
+
+$article_id = 324; /* Temp article */
 
 /* Queries specificed article */
 $sql = "SELECT * 
@@ -17,17 +21,39 @@ $article = $mysqli->query($sql);
 
 $metadata = $article->fetch_assoc();
 
-$citation = "%0 $metadata[type]
-			%T $metadata[title]
-			%A $metadata[authors]
-			%J $metadata[j_name]
-			%V $metadata[j_volume]
-			%N $metadata[j_issue]
-			%P $metadata[startpage]-$metadata[endpage]
-			%@ $metadata[isbn_issn]
-			%D $metadata[a_date]";
+/* Splits up mulitple authors */
+$author_array = explode(";", $metadata['authors']);
+$authors = "";
+foreach($author_array as $author) {
+	$authors .= "%A " . $author . "<br>"; 
+}
+
+$enw = "%0 $metadata[type]<br>" .
+			"%T $metadata[title]<br>" .
+			$authors .
+			"%J $metadata[j_name]<br>" .
+			"%V $metadata[j_volume]<br>" .
+			"%N $metadata[j_issue]<br>" .
+			"%P $metadata[startpage]-$metadata[endpage]<br>" .
+			"%@ $metadata[isbn_issn]<br>" .
+			"%D $metadata[a_date]";
 
 /* Removes any tabs and prints results to enw file */
-$citation = preg_replace('/\t/', '', $citation);
-print $citation;
+$enw = preg_replace('/\t/', '', $enw);
+
+/* Create MLA citation for journal article */
+if ($metadata['type'] == 'JOUR') {
+	$mla_journ = 
+		str_replace(";", ", ", $metadata['authors']) . 
+		"\"$metadata[title].\" " . 
+		"<i>$metadata[j_name]</i> " .
+		"$metadata[j_volume]." . 
+		"$metadata[j_issue]." . 
+		"($metadata[a_date]): " .
+		"$metadata[startpage]-$metadata[endpage]. " .
+		"Print.";
+}
+
+$data = array("enw" => $enw, "mla_journ" => $mla_journ);
+echo json_encode($data);
 ?>
