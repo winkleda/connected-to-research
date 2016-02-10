3fb5e7881571d568b73fd974b9bfb0ecd7a680a8
Christopher Nguyen <nguyenc5@flip2.engr.oregonstate.edu>
2016-02-10 14:53:51 -0800

    grants parser completely done

--- scripts/connection.php
+++ scripts/connection.php
@@ -4,11 +4,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 $dbhost = 'oniddb.cws.oregonstate.edu';
-$dbname = 'winkleda-db';
-$dbuser = 'winkleda-db';
-
-/* password.php only contains $dbpass, the password for winkleda-db */
-include('password.php');
+$dbname = 'nguyenc5-db';
+$dbuser = 'nguyenc5-db';
+$dbpass = '61WpUnCnU7pDUl8g';
 
 $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
@@ -16,7 +14,8 @@ if($mysqli->connect_errno) {
 	//echo "Connection failure: " . $mysqli->connect_error;
 	die('<div class="alert alert-danger" role="alert">Fatal Error: could not connect to database: ' . $mysqli->connect_error . '</div>');
 }
-//else
+//else{
 //	echo "Connection Success! <br>";
+//}
 
 ?>
