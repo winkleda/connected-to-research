06755e473b526be1f433b9e3a8b9f0a2c97cf4fc
Benny Zhao <zhaoben777@gmail.com>
2016-02-10 00:51:36 -0800

    Fixed the Notice Type, Post Date, and Due Date for filter-funding.php, and gave the filter column some more space in funding.html.

--- ajax/filter-funding.php
+++ ajax/filter-funding.php
@@ -31,22 +31,32 @@ $agency_name = "SELECT agency, count(*) as count
 //The notice type for FBO
 $funding_noticeFBO = "SELECT notice_type, count(*) as count
         FROM ctr_funding_fbo
-        GROUP BY notice_type";
+        GROUP BY notice_type
+        ORDER BY count DESC
+        LIMIT 0, 10";
 
 //The notice type for Grants
 $funding_noticeGrants = "SELECT instrument_type, count(*) as count
         FROM ctr_funding_grants
-        GROUP BY instrument_type";
+        GROUP BY instrument_type
+        ORDER BY count DESC
+        LIMIT 0, 10";
 
 //Posted Date
-$posted_date = "SELECT count(*) as count
+$posted_date = "SELECT post_date, count(*) as count
         FROM ctr_funding_base
-        WHERE YEAR(post_date) = ?";
+        GROUP BY post_date
+        ORDER BY count DESC
+        LIMIT 0, 5";
+        //WHERE YEAR(post_date) = ?";
 
 //Due Date
-$due_date = "SELECT count(*) as count
+$due_date = "SELECT due_date, count(*) as count
         FROM ctr_funding_base
-        WHERE due_date = ?";
+        GROUP BY due_date
+        ORDER BY count DESC
+        LIMIT 0, 5";
+        //WHERE due_date = ?";
 
 
 //assigning key => values to an array set to fields
@@ -84,6 +94,7 @@ if($stmt->prepare($funding_sourceGrants)){
 $agencyArray = array();
 $agencyPrefix = "agency=";
 
+//filter for All Agencies
 if($stmt->prepare($funding_agency_total)){
     
     $stmt->execute();
@@ -102,7 +113,7 @@ if($stmt->prepare($funding_agency_total)){
         } 
     }
 }
-    
+//filter for specific names    
 if($stmt->prepare($agency_name)){
     
     $stmt->execute();
@@ -120,43 +131,81 @@ if($stmt->prepare($agency_name)){
     } 
 }
 
+$noticeArray = array();
+$noticePrefix = "notice=";
+
+//filter for notice types in FBO
 if($stmt->prepare($funding_noticeFBO)){
     
     $stmt->execute();
     $result = $stmt->get_result();
+    
     if($result !== false) {
-        $columns = $result->fetch_assoc();
-        $count_value["noticeFBO"] = $columns['count'];
-    }
+        while(($columns = $result->fetch_assoc())) {
+            $notice = array(
+                "groupItem" => $columns["notice_type"],
+                "amount" => $columns["count"],
+                "filterName" => $noticePrefix . $columns["notice_type"]
+            );
+            array_push($noticeArray, $notice);
+        }   
+    } 
 }
+//filter for notice types in Grants
 if($stmt->prepare($funding_noticeGrants)){
     
     $stmt->execute();
     $result = $stmt->get_result();
-    if($result !== false) {
-        $columns = $result->fetch_assoc();
-        $count_value["noticeGrants"] = $columns['count'];
-    }
     
+    if($result !== false) {
+        while(($columns = $result->fetch_assoc())) {
+            $notice = array(
+                "groupItem" => $columns["instrument_type"],
+                "amount" => $columns["count"],
+                "filterName" => $noticePrefix . $columns["instrument_type"]
+            );
+            array_push($noticeArray, $notice);
+        }   
+    } 
 }
+
+$postDateArray = array();
+$dueDateArray = array();
+$postDatePrefix = "postDate=";
+$dueDatePrefix = "dueDate=";
+
+//filter by post date
 if($stmt->prepare($posted_date)){
     
-    $stmt->bind_param("s", $post_date);
     $stmt->execute();
     $result = $stmt->get_result();
+    
     if($result !== false) {
-        $columns = $result->fetch_assoc();
-        $count_value["postedDate"] = $columns['count'];
-    }
+        while(($columns = $result->fetch_assoc())) {
+            $postDate = array(
+                "groupItem" => $columns["post_date"],
+                "amount" => $columns["count"],
+                "filterName" => $postDatePrefix . $columns["post_date"]
+            );
+            array_push($postDateArray, $postDate);
+        }   
+    } 
 }
+//filter by due date
 if($stmt->prepare($due_date)){
     
-    $stmt->bind_param("s", $due_date);
     $stmt->execute();
     $result = $stmt->get_result();
+    
     if($result !== false) {
-        $columns = $result->fetch_assoc();
-        $count_value["dueDate"] = $columns['count'];
+        while(($columns = $result->fetch_assoc())) {
+            $dueDate = array(
+                "groupItem" => $columns["due_date"],
+                "amount" => $columns["count"],
+                "filterName" => $dueDatePrefix . $columns["due_date"]
+            );
+            array_push($dueDateArray, $dueDate);
+        }   
     } 
 }
 
@@ -183,33 +232,18 @@ $data = array(
 	),
     array(
 		"header" => "Notice Type",
-		"items" => array(
-			array(
-				"groupItem" => "FedBizOpps",
-				"amount" => $count_value['noticeFBO'],
-				"filterName" => "noticeFBO"
-			),
-			array(
-				"groupItem" => "Grants",
-				"amount" => $count_value['noticeGrants'],
-				"filterName" => "noticeGrants"
-			)
-		)
+		"items" =>
+            $noticeArray
 	),
     array(
-		"header" => "Date",
-		"items" => array(
-			array(
-				"groupItem" => "Posted Date",
-				"amount" => $count_value['postedDate'],
-				"filterName" => "postedDate"
-			),
-			array(
-				"groupItem" => "Due Date",
-				"amount" => $count_value['dueDate'],
-				"filterName" => "dueDate"
-			)
-		)
+		"header" => "Post Date",
+		"items" =>
+            $postDateArray
+	),
+    array(
+		"header" => "Due Date",
+		"items" =>
+            $dueDateArray
 	),
 );
 
