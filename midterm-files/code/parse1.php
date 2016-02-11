231992170050131e1eb99d2683926411be1c5616
David D. Winkler <winkleda@onid.oregonstate.edu>
2016-02-10 21:01:49 -0800

    Added files that the midterm need to compile.

---
+++ midterm-files/code/parse1.php
@@ -0,0 +1,12 @@
+while($xml->read())
+{
+    // Read each element, skipping any that aren't interesting
+    if(!($xml->nodeType == XMLReader::ELEMENT)
+    || (!in_array($xml->name, $types)))
+        continue;
+
+    // echo "<br><br>&nbsp;&nbsp;&nbsp;" . $xml->name . '<br>';
+    $node = new SimpleXMLElement($xml->readOuterXML());
+
+    echo "NOTICE TYPE: " . clean_string($xml->name, $mysqli) . ". SOLNBR: " . $node->SOLNBR . "<br>";
+...
