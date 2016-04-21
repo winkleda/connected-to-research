ee4e21e7d66a85488a057e05a7270ee4c6aee7c4
David D. Winkler <winkleda@onid.oregonstate.edu>
2016-04-21 15:37:30 -0700

    Updated document files for TA's comments and also updated packages

--- document-files/code/parse1.php
+++ document-files/code/parse1.php
@@ -1,13 +1,13 @@
-while($xml->read())
-{
-    // Read each element, skipping any that aren't interesting
-    if(!($xml->nodeType == XMLReader::ELEMENT)
-    || (!in_array($xml->name, $types)))
-        continue;
+    while($xml->read())
+    {
+        // Read each element, skipping any that aren't interesting
+        if(!($xml->nodeType == XMLReader::ELEMENT)
+        || (!in_array($xml->name, $types)))
+            continue;
 
-    // echo "<br><br>&nbsp;&nbsp;&nbsp;" . $xml->name . '<br>';
-    $node = new SimpleXMLElement($xml->readOuterXML());
+        // echo "<br><br>&nbsp;&nbsp;&nbsp;" . $xml->name . '<br>';
+        $node = new SimpleXMLElement($xml->readOuterXML());
 
-    echo "NOTICE TYPE: " . clean_string($xml->name, $mysqli) . ". SOLNBR: "
-    . $node->SOLNBR . "<br>";
-...
+        echo "NOTICE TYPE: " . clean_string($xml->name, $mysqli) . ". SOLNBR: "
+        . $node->SOLNBR . "<br>";
+    ...
--- document-files/code/parse2.php
+++ document-files/code/parse2.php
@@ -1,7 +1,7 @@
-            // Try to find all elements that matter to our database
-            if(strncmp($line, "<DATE>", strlen("<DATE>")) == 0)
-                $date = $mysqli->escape_string(str_replace("<DATE>", "", $line));
-            if(strncmp($line, "<YEAR>", strlen("<YEAR>")) == 0)
-                $year = $mysqli->escape_string(str_replace("<YEAR>", "", $line));
-            if(strncmp($line, "<AGENCY>", strlen("<AGENCY>")) == 0)
-                $agency = $mysqli->escape_string(str_replace("<AGENCY>", "", $line));
+    // Try to find all elements that matter to our database
+    if(strncmp($line, "<DATE>", strlen("<DATE>")) == 0)
+        $date = $mysqli->escape_string(str_replace("<DATE>", "", $line));
+    if(strncmp($line, "<YEAR>", strlen("<YEAR>")) == 0)
+        $year = $mysqli->escape_string(str_replace("<YEAR>", "", $line));
+    if(strncmp($line, "<AGENCY>", strlen("<AGENCY>")) == 0)
+        $agency = $mysqli->escape_string(str_replace("<AGENCY>", "", $line));
